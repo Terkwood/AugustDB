99de8aa6e5f3e9c43a2fca091fed3bbd54beb1c2
terkwood <38859656+Terkwood@users.noreply.github.com>
2021-07-20 17:28:11 -0400

    nest too deeply

--- lib/sstable.ex
+++ lib/sstable.ex
@@ -1,15 +1,25 @@
 NimbleCSV.define(SSTableParser, separator: "\t", escape: "\"")
 
 defmodule SSTable do
+  defstruct [:index, :table]
+
   def dump(keyvals) when is_list(keyvals) do
-    s = SSTableParser.dump_to_stream([keyvals])
+    s = SSTableParser.dump_to_stream(keyvals)
 
-    for everything <- s do
-      skip_separators = Enum.drop_every([nil | everything], 2)
+    index =
+      List.flatten(
+        for everything <- s do
+          skip_separators = Enum.drop_every([nil | everything], 2)
 
-      for row <- skip_separators do
-        IO.puts("#{row} length #{String.length(row)}")
-      end
-    end
+          {is, _acc} =
+            Enum.map_reduce(skip_separators, 0, fn row, acc ->
+              {{row, acc}, String.length(row) + acc}
+            end)
+
+          is
+        end
+      )
+
+    %__MODULE__{index: index, table: s}
   end
 end
