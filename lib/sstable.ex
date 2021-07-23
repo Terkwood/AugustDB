b67f027e16d25eec0216ca79c08ad6ef0a408d4d
terkwood <38859656+Terkwood@users.noreply.github.com>
2021-07-23 12:58:22 -0400

    feat: seek to key-value in file

--- lib/sstable.ex
+++ lib/sstable.ex
@@ -4,7 +4,9 @@ defmodule SSTable do
   defstruct [:index, :table]
 
   @csv_header [["k", "v"]]
+  @csv_header_string "k\tv\n"
   @csv_header_bytes 4
+  @csv_row_separator "\n"
 
   @spec dump(maybe_improper_list) :: %SSTable{index: list, table: list}
   @doc """
@@ -41,6 +43,41 @@ defmodule SSTable do
       Enum.map_reduce(rlens, @csv_header_bytes, fn {key, l}, acc -> {{key, acc}, acc + l} end)
 
     %__MODULE__{index: index, table: Stream.concat(csv_header, csv_stream)}
+  end
+
+  def seek(file_name, _key, offset \\ 0) do
+    {:ok, file} = :file.open(file_name, [:read, :binary])
+    :file.position(file, offset)
+    SSTableParser.parse_string(@csv_header_string <> keep_reading(file))
+  end
+
+  @seek_bytes 64
+  defp keep_reading(file, acc \\ "") do
+    case :file.read(file, @seek_bytes) do
+      {:ok, data} ->
+        case stop_at_row_separator(data) do
+          :continue -> keep_reading(file, acc <> data)
+          {:stop, up_to_sep} -> acc <> up_to_sep
+        end
+
+      :eof ->
+        acc
+    end
+  end
+
+  defp stop_at_row_separator(data) do
+    IO.inspect(data)
+
+    case String.split(data, @csv_row_separator) do
+      [_just_one] ->
+        :continue
+
+      [] ->
+        {:stop, ""}
+
+      many ->
+        {:stop, hd(many)}
+    end
   end
 
   defp row_length(row) when is_list(row) do
