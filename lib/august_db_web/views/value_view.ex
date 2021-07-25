df905986f88a5a2d3c0156a1dfd29c6e32544509
Terkwood <38859656+Terkwood@users.noreply.github.com>
2021-07-25 08:46:26 -0400

    feat: HTTP GET (#26)

--- lib/august_db_web/views/value_view.ex
+++ lib/august_db_web/views/value_view.ex
@@ -1,5 +1,14 @@
 defmodule AugustDbWeb.ValueView do
   use AugustDbWeb, :view
+  alias AugustDbWeb.ValueView
+
+  def render("show.json", %{value: data}) do
+    render_one(data, ValueView, "value.json")
+  end
+
+  def render("value.json", %{value: data}) do
+    data
+  end
 
   def render("update.json", %{key: key, value: value}) do
     %{key: key, value: value}
