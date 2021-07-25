d51acc733d90b1fa84b80e91c1c6c412d138eedb
Terkwood <38859656+Terkwood@users.noreply.github.com>
2021-07-25 13:31:15 -0400

    feat: current and flushing memtable (#28)

--- lib/august_db/memtable.ex
+++ lib/august_db/memtable.ex
@@ -1,12 +1,19 @@
 defmodule Memtable do
   use Agent
 
+  defstruct current: :gb_trees.empty(), flushing: :gb_trees.empty()
+
   def start_link(initial_value) do
     Agent.start_link(fn -> initial_value end, name: __MODULE__)
   end
 
   def query(key) do
-    case Agent.get(__MODULE__, fn tree -> :gb_trees.lookup(key, tree) end) do
+    case Agent.get(__MODULE__, fn %__MODULE__{current: current, flushing: flushing} ->
+           case :gb_trees.lookup(key, current) do
+             :none -> :gb_trees.lookup(key, flushing)
+             some -> some
+           end
+         end) do
       {:value, {:value, data, time}} ->
         {:value, data, time}
 
@@ -19,14 +26,27 @@ defmodule Memtable do
   end
 
   def update(key, value) do
-    Agent.update(__MODULE__, fn tree ->
-      :gb_trees.enter(key, {:value, value, System.monotonic_time()}, tree)
+    IO.inspect(key)
+    IO.inspect(value)
+
+    Agent.update(__MODULE__, fn %__MODULE__{current: current, flushing: flushing} ->
+      %__MODULE__{
+        current: :gb_trees.enter(key, {:value, value, System.monotonic_time()}, current),
+        flushing: flushing
+      }
     end)
   end
 
   def delete(key) do
-    Agent.update(__MODULE__, fn tree ->
-      :gb_trees.enter(key, {:tombstone, System.monotonic_time()}, tree)
+    Agent.update(__MODULE__, fn %__MODULE__{current: current, flushing: flushing} ->
+      %__MODULE__{
+        current: :gb_trees.enter(key, {:tombstone, System.monotonic_time()}, current),
+        flushing: flushing
+      }
     end)
+  end
+
+  def flush() do
+    raise "todo"
   end
 end
