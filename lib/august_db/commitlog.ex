57adcabd5e0f8d3a899f9d52362c633ed39b4eee
terkwood <38859656+Terkwood@users.noreply.github.com>
2021-07-29 10:52:16 -0400

    fix arg

--- lib/august_db/commitlog.ex
+++ lib/august_db/commitlog.ex
@@ -1,12 +1,12 @@
 defmodule CommitLog do
-  # @tombstone_string Tombstone.string()
+  @tombstone_string Tombstone.string()
   @log_file "commit.log"
 
-  # def append(key, :tombstone) do
-  #  __MODULE__.append(key, @tombstone_string)
-  # end
+  def append(key, :tombstone) do
+    __MODULE__.append(key, @tombstone_string)
+  end
 
   def append(key, value) do
-    File.write!(@log_file, key <> "\t" <> value <> "\n", :append)
+    File.write!(@log_file, key <> "\t" <> value <> "\n", [:append])
   end
 end
