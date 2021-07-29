bed9452a3623d81437c221d527cec2a7d4a4405a
Terkwood <38859656+Terkwood@users.noreply.github.com>
2021-07-29 11:18:44 -0400

    feat: append to commit log (#42)

---
+++ lib/august_db/commitlog.ex
@@ -0,0 +1,22 @@
+NimbleCSV.define(CommitLogParser, separator: "\t", escape: "\"")
+
+defmodule CommitLog do
+  #  @csv_header [["k", "v", "t"]]
+  #  @csv_header_string "k\tv\tt\n"
+  #  @csv_header_bytes 6
+  #  @csv_row_separator "\n"
+  @tombstone_string Tombstone.string()
+  @log_file "commit.log"
+
+  def append(key, :tombstone) do
+    __MODULE__.append(key, @tombstone_string)
+  end
+
+  def append(key, value) do
+    File.write!(
+      @log_file,
+      key <> "\t" <> value <> "\t" <> "#{:erlang.monotonic_time()}" <> "\n",
+      [:append]
+    )
+  end
+end
---
+++ lib/august_db/tombstone.ex
@@ -0,0 +1,5 @@
+defmodule Tombstone do
+  def string() do
+    "$T$"
+  end
+end
