81d0867ee9f72fdfd9a10e0cacf50e4df305bf42
terkwood <38859656+Terkwood@users.noreply.github.com>
2021-07-29 10:57:15 -0400

    define csv parser

--- lib/august_db/commitlog.ex
+++ lib/august_db/commitlog.ex
@@ -1,4 +1,10 @@
+NimbleCSV.define(CommitLogParser, separator: "\t", escape: "\"")
+
 defmodule CommitLog do
+  @csv_header [["k", "v", "t"]]
+  @csv_header_string "k\tv\tt\n"
+  @csv_header_bytes 6
+  @csv_row_separator "\n"
   @tombstone_string Tombstone.string()
   @log_file "commit.log"
 
