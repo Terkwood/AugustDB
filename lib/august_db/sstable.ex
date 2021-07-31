06eceac2e131817a1f69d0fe98b71d05abb577b4
terkwood <38859656+Terkwood@users.noreply.github.com>
2021-07-31 19:12:13 -0400

    fix: represent sst index as map

--- lib/august_db/sstable.ex
+++ lib/august_db/sstable.ex
@@ -16,11 +16,11 @@ defmodule SSTable do
 
       iex> them = SSTable.dump([~w(k1 v), ~w(k2 ww), ~w(k3 uuu)])
       iex> them.index
-      [
-        {"k1", 4},
-        {"k2", 9},
-        {"k3", 15},
-      ]
+      %{
+        "k1" => 4,
+        "k2" => 9,
+        "k3" => 15,
+      }
 
       iex> them = SSTable.dump([~w(k1 v), ~w(k2 ww), ~w(k3 uuu)])
       iex> IO.iodata_to_binary(Enum.to_list(them.table))
