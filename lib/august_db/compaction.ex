3f778b6341b3a3d8a45be53e382008906cf2267e
terkwood <38859656+Terkwood@users.noreply.github.com>
2021-07-31 14:34:55 -0400

    open files

--- lib/august_db/compaction.ex
+++ lib/august_db/compaction.ex
@@ -10,8 +10,15 @@ defmodule Compaction do
     raise "todo"
   end
 
-  defp merge(_older, _newer) do
-    raise "todo"
+  defp merge(older_path, _newer_path) do
+    older_sst = :file.open(older_path, [:read, :binary, {:read_ahead, 100_000}])
+    newer_sst = :file.open(older_path, [:read, :binary, {:read_ahead, 100_000}])
+
+    raise "merge them"
+
+    :file.close(older_sst)
+    :file.close(newer_sst)
+    raise "close output  file"
 
     raise "return the path of the output file"
   end
