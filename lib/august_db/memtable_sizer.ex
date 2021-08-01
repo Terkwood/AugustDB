91f5aee46ee1f8399abe09ded96a92e07058b90c
terkwood <38859656+Terkwood@users.noreply.github.com>
2021-08-01 10:46:28 -0400

    feat: track memtable size

---
+++ lib/august_db/memtable_sizer.ex
@@ -0,0 +1,15 @@
+defmodule Memtable.Sizer do
+  use GenServer
+
+  defmodule State do
+    defstruct total_size: 0, size_per_key: %{}
+  end
+
+  def start_link(_opts) do
+    GenServer.start_link(__MODULE__, %State{})
+  end
+
+  def init(state) do
+    {:ok, state}
+  end
+end
