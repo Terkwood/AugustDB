da535cff6de57701de459524bf767b7153e039fe
terkwood <38859656+Terkwood@users.noreply.github.com>
2021-08-01 17:59:42 -0400

    stub query

--- lib/august_db/sstable.ex
+++ lib/august_db/sstable.ex
@@ -2,6 +2,41 @@ defmodule SSTable do
   defstruct [:index, :table]
 
   @doc """
+  Query all SSTable files using their associated index file and a key,
+  returning a value if present. Filters tombstone entries.
+
+  There must be an associated `<timestamp>.idx` file present for each SSTable,
+  or the private query function will fail.
+
+  This function returns `:tombstone` in the case of deleted entries.
+
+  ## Examples
+
+  Basic query
+
+  ```elixir
+  SSTable.query_all("a")
+  ```
+
+  Combined with Memtable
+
+  ```elixir
+  Memtable.update("bar","BAZ"); Memtable.delete("foo"); Memtable.flush()
+  :ok
+  SSTable.query_all("bar")
+  "BAZ"
+  SSTable.query_all("foo")
+  :tombstone
+  SSTable.query_all("a")
+  :none
+  ```
+  """
+  def query_all(key) do
+    sst_files = Path.wildcard("*.sst") |> Enum.sort() |> Enum.reverse()
+    query_all(key, sst_files)
+  end
+
+  @doc """
   Write a list of key/value pairs to binary SSTable file
   Also write an index of offsets.
 
@@ -29,8 +64,6 @@ defmodule SSTable do
 
     kvs = Enum.filter(maybe_kvs, &(&1 != nil))
 
-    IO.inspect(kvs)
-
     time_name = "#{:erlang.system_time()}"
 
     table_fname = "#{time_name}.sst"
@@ -38,10 +71,27 @@ defmodule SSTable do
     {:ok, sst_out_file} = :file.open(table_fname, [:raw, :append])
 
     idx = kvs |> write_binary_idx(sst_out_file)
-    IO.inspect(idx)
 
     index_path = "#{time_name}.idx"
     File.write!(index_path, :erlang.term_to_binary(idx))
+
+    IO.puts("Dumped SSTable to #{table_fname}")
+  end
+
+  defp query_all(_key, []) do
+    :none
+  end
+
+  defp query_all(key, [sst_file | rest]) do
+    case query(key, sst_file) do
+      :none -> query_all(key, rest)
+      :tombstone -> :tombstone
+      value -> value
+    end
+  end
+
+  defp query(key, sst_file_or_timestamp) do
+    raise "todo"
   end
 
   defp write_binary_idx(pairs, device, acc \\ {0, %{}})
