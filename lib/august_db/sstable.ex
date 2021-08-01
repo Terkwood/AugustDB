7416a33c27b9f699cdeffc81d3e54d2c72b47595
terkwood <38859656+Terkwood@users.noreply.github.com>
2021-08-01 17:54:25 -0400

    dump seems to work

--- lib/august_db/sstable.ex
+++ lib/august_db/sstable.ex
@@ -21,13 +21,13 @@ defmodule SSTable do
     maybe_kvs =
       for entry <- :gb_trees.to_list(gb_tree) do
         case entry do
-          {key, {:value, value, _time}} -> [key, value]
-          {key, {:tombstone, _time}} -> [key, :tombstone]
+          {key, {:value, value, _time}} -> {key, value}
+          {key, {:tombstone, _time}} -> {key, :tombstone}
           _ -> nil
         end
       end
 
-    kvs = Enum.filter(maybe_kvs, &(&1 != nil)) |> Enum.flat_map(fn x -> x end)
+    kvs = Enum.filter(maybe_kvs, &(&1 != nil))
 
     IO.inspect(kvs)
 
@@ -49,22 +49,29 @@ defmodule SSTable do
   defp write_binary_idx([{key, value} | rest], device, acc) do
     ks = byte_size(key)
 
-    segment =
+    segment_size =
       case value do
         :tombstone ->
-          IO.puts(device, <<ks::64>>)
-          IO.puts(device, <<@tombstone::64>>)
-          IO.puts(device, key)
+          kl = <<ks::64>>
+          vl = <<@tombstone::64>>
+          :ok = :file.write(device, kl)
+          :ok = :file.write(device, vl)
+          :ok = :file.write(device, key)
+          byte_size(kl) + byte_size(vl) + byte_size(key)
 
         bin when is_binary(bin) ->
           vs = byte_size(bin)
-          <<ks::64, vs::64>> <> key <> bin
+          kvl = <<ks::64, vs::64>>
+          :ok = :file.write(device, kvl)
+          :ok = :file.write(device, key)
+          :ok = :file.write(device, bin)
+          byte_size(kvl) + byte_size(key) + byte_size(bin)
       end
 
     {al, idx} = acc
-    next_len = al + byte_size(segment)
+    next_len = al + segment_size
 
-    write_binary_idx(device, rest, {next_len, Map.put(idx, key, al)})
+    write_binary_idx(rest, device, {next_len, Map.put(idx, key, al)})
   end
 
   defp write_binary_idx([], _device, {_byte_pos, idx}) do
