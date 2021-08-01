0eef6ee9decdc535ca8ff5bc17691d6b1a0bf5fc
terkwood <38859656+Terkwood@users.noreply.github.com>
2021-08-01 16:51:05 -0400

    stub out funcs

--- lib/august_db/sstable.ex
+++ lib/august_db/sstable.ex
@@ -1,4 +1,26 @@
 defmodule SSTable do
+  @doc """
+  Dump a list of key/value pairs to an IO-ready binary stream, accompanied by an index of offsets.
+
+  ## Example
+
+      iex> them = SSTable.dump([~w(k1 v), ~w(k2 ww), ~w(k3 uuu)])
+      iex> them.index
+      %{
+        "k1" => 4,
+        "k2" => 9,
+        "k3" => 15,
+      }
+
+      iex> them = SSTable.dump([~w(k1 v), ~w(k2 ww), ~w(k3 uuu)])
+      iex> raise "todo"
+      iex> IO.iodata_to_binary(Enum.to_list(them.table))
+      ???
+  """
+  def dump(keyvals) when is_list(keyvals) do
+    raise "todo"
+  end
+
   def from(memtable) do
     raise "todo"
   end
