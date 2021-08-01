4fc55182a861ea71899afbb6baf7b03c237c806e
terkwood <38859656+Terkwood@users.noreply.github.com>
2021-08-01 11:39:40 -0400

    write resize cast

--- lib/august_db/memtable_sizer.ex
+++ lib/august_db/memtable_sizer.ex
@@ -2,7 +2,7 @@ defmodule Memtable.Sizer do
   use GenServer
 
   defmodule State do
-    defstruct total_size: 0, kv_size: %{}
+    defstruct total_size: 0, sizes: %{}
   end
 
   def start_link(_) do
@@ -11,5 +11,19 @@ defmodule Memtable.Sizer do
 
   def init(state) do
     {:ok, state}
+  end
+
+  def handle_cast({:resize, key, kv_size}, state) do
+    old_kv_size =
+      case Map.get(state.sizes, key) do
+        nil -> 0
+        n -> n
+      end
+
+    kv_size_diff = kv_size - old_kv_size
+
+    new_total_size = state.total_size + kv_size_diff
+
+    {:noreply, %State{total_size: new_total_size, sizes: Map.put(state.sizes, key, kv_size)}}
   end
 end
