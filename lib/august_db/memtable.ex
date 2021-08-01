d1687fff99897259ba34aa1af0e87bb7e9c9a9b7
terkwood <38859656+Terkwood@users.noreply.github.com>
2021-08-01 17:40:03 -0400

    Draft

--- lib/august_db/memtable.ex
+++ lib/august_db/memtable.ex
@@ -70,19 +70,6 @@ defmodule Memtable do
     commit_log_backup = CommitLog.backup()
     CommitLog.new()
 
-    sstable = SSTable.from(flushing)
-
-    time_name = "#{:erlang.system_time()}"
-
-    table_fname = "#{time_name}.sst"
-    table_file_stream = File.stream!(table_fname)
-    table_stream = sstable.table
-    table_stream |> Stream.into(table_file_stream) |> Stream.run()
-
-    index_binary = :erlang.term_to_binary(sstable.index)
-    index_path = "#{time_name}.idx"
-    File.write!(index_path, index_binary)
-
     # Finished.  Clear the flushing table state.
     Agent.update(__MODULE__, fn %__MODULE__{current: current, flushing: _} ->
       %__MODULE__{
