3bb03a8242fde3e897d9dd25ed15cdba20a24b60
terkwood <38859656+Terkwood@users.noreply.github.com>
2021-08-03 14:18:41 -0400

    use a power of 2

--- lib/august_db/sstable_index.ex
+++ lib/august_db/sstable_index.ex
@@ -1,5 +1,6 @@
 defmodule SSTable.Index do
-  @bytes_per_entry 3 * 1024
+  @bytes_per_entry 4 * 1024
+
   @doc """
   Defines how sparse the index file should be.
   The application will only write one `key -> offset`
