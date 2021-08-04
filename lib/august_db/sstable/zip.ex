91158bb45ae3e617ab4441e982f290f8223cc135
terkwood <38859656+Terkwood@users.noreply.github.com>
2021-08-04 18:51:39 -0400

    stub test

--- lib/august_db/sstable/zip.ex
+++ lib/august_db/sstable/zip.ex
@@ -3,9 +3,17 @@ defmodule SSTable.Zip do
     <<:erlang.crc32(uncompressed_payload)::32>>
   end
 
+  @doc """
+  Verifies a gzip checksum, per the
+  [gzip file format](https://en.wikipedia.org/wiki/Gzip#File_format):
+
+  > an 8-byte footer, containing a CRC-32 checksum and the
+  > length of the original uncompressed data, modulo 2^32
+  """
   def verify_checksum(compressed_payload, crc32_checksum) do
     pl = :binary.bin_to_list(compressed_payload)
 
+    # not sure why we had to reverse this... but it works?!
     payload_checksum = Enum.reverse(pl) |> Enum.drop(4) |> Enum.take(4)
 
     case payload_checksum == :binary.bin_to_list(crc32_checksum) do
---
+++ test/sstable/zip_test.exs
@@ -0,0 +1,7 @@
+# defmodule ZipTest do
+#   use ExUnit.Case, async: true
+
+#   test "crc check works" do
+#     assert
+#   end
+# end
