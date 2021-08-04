b770b87ac65365b7c51952cd0f6d0525766d6d2a
terkwood <38859656+Terkwood@users.noreply.github.com>
2021-08-04 06:27:32 -0400

    chore: set version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule AugustDb.MixProject do
   def project do
     [
       app: :august_db,
-      version: "0.3.0-a",
+      version: "0.3.0",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
