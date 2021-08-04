24ab72e85d52a626089203f0dfdcc532920d88af
terkwood <38859656+Terkwood@users.noreply.github.com>
2021-08-04 06:04:22 -0400

    init code

--- lib/august_db/application.ex
+++ lib/august_db/application.ex
@@ -11,10 +11,11 @@ defmodule AugustDb.Application do
       {Memtable, %Memtable{}},
       # Start Memtable Size tracker
       Memtable.Sizer,
-      # Make sure commit log exists, old entries are written into SSTable, etc.
-      {Task, fn -> Startup.init() end},
       # Start the SSTable Index agent
       {SSTable.Index, %{}},
+      # Make sure commit log exists, old entries are written into SSTable, etc.
+      {Task, fn -> Startup.init() end},
+
       # Start periodic SSTable compaction
       SSTable.Compaction.Periodic,
       # Start the Telemetry supervisor
