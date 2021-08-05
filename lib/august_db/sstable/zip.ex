73223ce00faf4d828323ee27d4cd8c417406721d
terkwood <38859656+Terkwood@users.noreply.github.com>
2021-08-05 16:50:30 -0400

    sort of weird

--- lib/august_db/sstable/zip.ex
+++ lib/august_db/sstable/zip.ex
@@ -33,49 +33,65 @@ defmodule SSTable.Zip do
                                           } ->
       kv_bin = SSTable.KV.to_binary(key, value)
 
+      {next_chunk_key, next_chunk_offset} =
+        case {chunk_key, chunk_offset} do
+          {nil, nil} -> {key, current_offset}
+          nck -> nck
+        end
+
       if byte_size(current_chunk) + byte_size(kv_bin) >= @uncompressed_data_chunk do
         gzip_chunk = :zlib.gzip(current_chunk <> kv_bin)
-        chunk_size = byte_size(gzip_chunk)
 
         %ChunkAccum{
           payload: payload <> gzip_chunk,
           current_chunk: <<>>,
-          index: [{chunk_key, chunk_offset} | index],
-          current_offset: chunk_size + current_offset
+          index: [next_chunk_key | index],
+          chunk_key: nil,
+          chunk_offset: nil,
+          current_offset: current_offset + byte_size(gzip_chunk)
+        }
+      else
+        %ChunkAccum{
+          payload: payload,
+          current_chunk: current_chunk <> kv_bin,
+          index: index,
+          chunk_key: next_chunk_key,
+          chunk_offset: next_chunk_offset,
+          current_offset: current_offset
         }
       end
     end)
   end
 
-  def create_gzipped_chunks_fail(kvs) do
-    {leftover, all_chunks, _, last_byte_pos, idx, last_key} =
-      kvs
-      |> Enum.reduce({<<>>, [], 0, 0, [], nil}, fn {key, value},
-                                                   {current_chunk, all_chunks, current_byte_pos,
-                                                    last_byte_pos, idx, last_key} ->
-        kv_bin = SSTable.KV.to_binary(key, value)
+  # def create_gzipped_chunks_fail(kvs) do
+  #   {leftover, all_chunks, _, last_byte_pos, idx, last_key} =
+  #     kvs
+  #     |> Enum.reduce({<<>>, [], 0, 0, [], nil}, fn {key, value},
+  #                                                  {current_chunk, all_chunks, current_byte_pos,
+  #                                                   last_byte_pos, idx, last_key} ->
+  #       kv_bin = SSTable.KV.to_binary(key, value)
 
-        keep_last_key =
-          case last_key do
-            nil -> key
-            l -> l
-          end
+  #       keep_last_key =
+  #         case last_key do
+  #           nil -> key
+  #           l -> l
+  #         end
 
-        # if last_byte_pos + payload_size >= SSTable.Settings.index_chunk_size() do
-        #   {<<>>, [gz_payload | all_chunks], next_byte_pos, current_byte_pos,
-        #    [{key, last_byte_pos} | idx], nil}
-        # else
-        #   {current_chunk <> more_payload, all_chunks, next_byte_pos, last_byte_pos, idx,
-        #    keep_last_key}
-        # end
-      end)
+  #       # if last_byte_pos + payload_size >= SSTable.Settings.index_chunk_size() do
+  #       #   {<<>>, [gz_payload | all_chunks], next_byte_pos, current_byte_pos,
+  #       #    [{key, last_byte_pos} | idx], nil}
+  #       # else
+  #       #   {current_chunk <> more_payload, all_chunks, next_byte_pos, last_byte_pos, idx,
+  #       #    keep_last_key}
+  #       # end
+  #     end)
 
-    case {leftover, last_key} do
-      {bin, k} when k != nil ->
-        {[bin | all_chunks], [{last_key, last_byte_pos} | idx]}
+  #   case {leftover, last_key} do
+  #     {bin, k} when k != nil ->
+  #       {[bin | all_chunks], [{last_key, last_byte_pos} | idx]}
 
-      _ ->
-        {all_chunks, idx}
-    end
-  end
+  #     _ ->
+  #       {all_chunks, idx}
+  #   end
+  # end
 end
