33be3b01b45f67188840991a1bf31122ffe3cf72
terkwood <38859656+Terkwood@users.noreply.github.com>
2021-08-05 16:41:13 -0400

    try making chunks

--- lib/august_db/sstable/zip.ex
+++ lib/august_db/sstable/zip.ex
@@ -4,22 +4,46 @@ defmodule SSTable.Zip do
   end
 
   defmodule ChunkAccum do
-    defstruct [:payload, :last_index, :current_offset]
+    defstruct [:payload, :current_chunk, :chunk_key, :chunk_offset, :index, :current_offset]
 
     def empty do
-      %__MODULE__{payload: <<>>, last_index: nil, current_offset: 0}
+      %__MODULE__{
+        payload: <<>>,
+        current_chunk: <<>>,
+        chunk_key: nil,
+        chunk_offset: 0,
+        index: [],
+        current_offset: 0
+      }
     end
   end
 
+  # wild guess at how much uncompressed data we should read in before gzipping
+  @uncompressed_data_chunk SSTable.Settings.index_chunk_size() * 64
   def zip(kvs) do
     kvs
-    |> Enum.reduce(ChunkAccum.empty(), fn %ChunkAccum{
+    |> Enum.reduce(ChunkAccum.empty(), fn {key, value},
+                                          %ChunkAccum{
                                             payload: payload,
-                                            last_index: last_index,
+                                            current_chunk: current_chunk,
+                                            chunk_key: chunk_key,
+                                            chunk_offset: chunk_offset,
+                                            index: index,
                                             current_offset: current_offset
-                                          },
-                                          acc ->
-      raise "todo"
+                                          } ->
+      kv_bin = SSTable.KV.to_binary(key, value)
+
+      if byte_size(current_chunk) + byte_size(kv_bin) >= @uncompressed_data_chunk do
+        gzip_chunk = :zlib.gzip(current_chunk <> kv_bin)
+        chunk_size = byte_size(gzip_chunk)
+
+        %ChunkAccum{
+          payload: payload <> gzip_chunk,
+          current_chunk: <<>>,
+          index: [{chunk_key, chunk_offset} | index],
+          current_offset: chunk_size + current_offset
+        }
+      end
     end)
   end
 
@@ -37,13 +61,13 @@ defmodule SSTable.Zip do
             l -> l
           end
 
-        if last_byte_pos + payload_size >= SSTable.Settings.index_chunk_size() do
-          {<<>>, [gz_payload | all_chunks], next_byte_pos, current_byte_pos,
-           [{key, last_byte_pos} | idx], nil}
-        else
-          {current_chunk <> more_payload, all_chunks, next_byte_pos, last_byte_pos, idx,
-           keep_last_key}
-        end
+        # if last_byte_pos + payload_size >= SSTable.Settings.index_chunk_size() do
+        #   {<<>>, [gz_payload | all_chunks], next_byte_pos, current_byte_pos,
+        #    [{key, last_byte_pos} | idx], nil}
+        # else
+        #   {current_chunk <> more_payload, all_chunks, next_byte_pos, last_byte_pos, idx,
+        #    keep_last_key}
+        # end
       end)
 
     case {leftover, last_key} do
