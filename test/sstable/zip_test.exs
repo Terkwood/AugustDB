441c9266d541812f4949b76ad01573f3e821acf5
terkwood <38859656+Terkwood@users.noreply.github.com>
2021-08-05 16:54:33 -0400

    add test

--- test/sstable/zip_test.exs
+++ test/sstable/zip_test.exs
@@ -1,1 +1,10 @@
-# todo
+defmodule ZipTest do
+  use ExUnit.Case, async: true
+
+  import SSTable.Zip
+
+  test "payload accumulates for small inputs" do
+    r = zip([{"a", "b"}, {"aa", "bb"}])
+    assert byte_size(r.payload) > 0
+  end
+end
