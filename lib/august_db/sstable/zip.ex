24e33169ba039fa5215ad2882ec6b948cb33ffff
terkwood <38859656+Terkwood@users.noreply.github.com>
2021-08-05 17:12:58 -0400

    trim dead

--- lib/august_db/sstable/zip.ex
+++ lib/august_db/sstable/zip.ex
@@ -73,36 +73,4 @@ defmodule SSTable.Zip do
         c
     end
   end
-
-  # def create_gzipped_chunks_fail(kvs) do
-  #   {leftover, all_chunks, _, last_byte_pos, idx, last_key} =
-  #     kvs
-  #     |> Enum.reduce({<<>>, [], 0, 0, [], nil}, fn {key, value},
-  #                                                  {current_chunk, all_chunks, current_byte_pos,
-  #                                                   last_byte_pos, idx, last_key} ->
-  #       kv_bin = SSTable.KV.to_binary(key, value)
-
-  #       keep_last_key =
-  #         case last_key do
-  #           nil -> key
-  #           l -> l
-  #         end
-
-  #       # if last_byte_pos + payload_size >= SSTable.Settings.index_chunk_size() do
-  #       #   {<<>>, [gz_payload | all_chunks], next_byte_pos, current_byte_pos,
-  #       #    [{key, last_byte_pos} | idx], nil}
-  #       # else
-  #       #   {current_chunk <> more_payload, all_chunks, next_byte_pos, last_byte_pos, idx,
-  #       #    keep_last_key}
-  #       # end
-  #     end)
-
-  #   case {leftover, last_key} do
-  #     {bin, k} when k != nil ->
-  #       {[bin | all_chunks], [{last_key, last_byte_pos} | idx]}
-
-  #     _ ->
-  #       {all_chunks, idx}
-  #   end
-  # end
 end
