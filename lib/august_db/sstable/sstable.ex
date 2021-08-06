227087ed6f74966c02e4ec7bf76d19135f8567e9
terkwood <38859656+Terkwood@users.noreply.github.com>
2021-08-06 18:29:42 -0400

    trim

--- lib/august_db/sstable/sstable.ex
+++ lib/august_db/sstable/sstable.ex
@@ -93,7 +93,6 @@ defmodule SSTable do
     kvs = Enum.filter(maybe_kvs, &(&1 != nil))
 
     {payload, sparse_index} = SSTable.Zip.zip(kvs)
-    IO.inspect(sparse_index)
 
     time = :erlang.system_time()
     sst_path = new_filename(time)
