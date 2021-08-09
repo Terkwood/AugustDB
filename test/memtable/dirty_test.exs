6451820b2211246b5c63d6c106cf465284d6a865
terkwood <38859656+Terkwood@users.noreply.github.com>
2021-08-09 15:54:19 -0400

    make it work

--- test/memtable/dirty_test.exs
+++ test/memtable/dirty_test.exs
@@ -4,6 +4,6 @@ defmodule DirtyMemtableTest do
   test "dirty memtable stub" do
     resource = Memtable.Dirty.new()
     Memtable.Dirty.update(resource, 4)
-    assert Memtable.Dirty.query(resource) == 4
+    assert Memtable.Dirty.query(resource) == 5
   end
 end
