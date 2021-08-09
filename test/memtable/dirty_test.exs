863236f262af09ceed62d5edf2e137dcc4de78de
terkwood <38859656+Terkwood@users.noreply.github.com>
2021-08-09 15:57:08 -0400

    expand test

--- test/memtable/dirty_test.exs
+++ test/memtable/dirty_test.exs
@@ -4,6 +4,15 @@ defmodule DirtyMemtableTest do
   test "dirty memtable stub" do
     resource = Memtable.Dirty.new()
     Memtable.Dirty.update(resource, 4)
+    assert Memtable.Dirty.query(resource) == 4
+
+    assert Task.await(
+             Task.async(fn ->
+               Memtable.Dirty.update(resource, 5)
+               Memtable.Dirty.query(resource)
+             end)
+           ) == 5
+
     assert Memtable.Dirty.query(resource) == 5
   end
 end
