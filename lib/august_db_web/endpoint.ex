d2940af1200a701f51058c6cb03d8e45bc8e3f5c
terkwood <38859656+Terkwood@users.noreply.github.com>
2021-08-10 13:11:22 -0400

    fix: trim sql dependencies

--- lib/august_db_web/endpoint.ex
+++ lib/august_db_web/endpoint.ex
@@ -32,7 +32,6 @@ defmodule AugustDbWeb.Endpoint do
     socket "/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket
     plug Phoenix.LiveReloader
     plug Phoenix.CodeReloader
-    plug Phoenix.Ecto.CheckRepoStatus, otp_app: :august_db
   end
 
   plug Phoenix.LiveDashboard.RequestLogger,
